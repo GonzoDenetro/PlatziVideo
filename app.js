22331bd9c6ac93b33d0d814f9b5a24e9b1cce530
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-12-22 15:52:05 -0600

    Modulos agregados

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js";
+
 const video = document.querySelector('.video');
 const buttonVideo = document.querySelector('.playPause-btn')
-
-
-//FORMA 1
-/* function MediaPlayer(obj){
-    this.name = obj.name;
-    this.media = obj.movie
-} */
-
-//FORMA 2
-function MediaPlayer({name, movie}){
-    this.name = name;
-    this.media = movie;
-}
-MediaPlayer.prototype.playVideo = function(){
-    if(this.media.paused){
-        this.media.play();    
-    }
-    else {
-        this.media.pause();
-    }
-}
 
 const player = new MediaPlayer({movie: video, name: "Video fav"})
 
